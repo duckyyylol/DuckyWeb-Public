d7cecda3d7513d70d80a35c2397b19556af2550f
ducky <asqrybot@gmail.com>
2026-01-14 02:58:24 -0500

    Meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,12 @@
 </script>
 
 <svelte:head>
+	<meta property="og:title" content="ducky 🦆" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://ducky.wiki" />
+    <meta property="og:description" content="designer ৹ community manager ৹ developer" />
+    <meta name="theme-color" content="#ffff76" />
+
 	<link rel="icon" href={favicon} />
 	<title>ducky 🦆</title>
 </svelte:head>
