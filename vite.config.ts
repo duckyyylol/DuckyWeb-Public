78518cd1c5e0e46a57ffbd3eb14fbb33d83926c9
ducky <asqrybot@gmail.com>
2026-01-14 03:52:02 -0500

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	// server: {
-	// 	allowedHosts: [
-	// 		"collection-dover-lamb-hook.trycloudflare.com"
-	// 	]
-	// },
+	server: {
+		allowedHosts: [
+			"collection-dover-lamb-hook.trycloudflare.com",
+			"ducky.wiki"
+		]
+	},
 	
 });
